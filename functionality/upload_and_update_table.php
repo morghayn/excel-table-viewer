009c39bbfc797aa95945fe92e8e6d17747540185
morghayn <morgan.nolan@hotmail.com>
2020-06-06 19:30:36 +0100

    Bug Patch: Apostrophes.
    Excel documents containing apostrophes in any cell would result in the INSERT query within upload_and_update_table functionality file.
    
    This has now been patched, two further issues have been identified:
    1) It's limited to insertion speeds of approximately 50-200 records a second.
    2) If (for a massive file) the insertion time takes longer than 120 seconds, it will result in a cut off and an error being thrown.
    
    I will suggest that an improvement to be made and the identified bugs to be ironed out.

--- functionality/upload_and_update_table.php
+++ functionality/upload_and_update_table.php
@@ -1,88 +1,85 @@
 <?php
 
-    // :: 13/02/2020 :: bit of a messy class, may need a clean at some stage (to improve maintainability)
-	$arr_file_types = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-	require '../vendor/autoload.php';
+// :: 13/02/2020 :: bit of a messy class, may need a clean at some stage (to improve maintainability)
+$arr_file_types = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+require '../vendor/autoload.php';
 
-    function convert_excel_to_sqlite()
+function convert_excel_to_sqlite()
+{
+    /**
+     * Variables
+     */
+    $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+    $reader->setReadDataOnly(TRUE);
+    $spreadsheet = $reader->load("../uploads/EAMs.xlsx");
+    $worksheet = $spreadsheet->getActiveSheet();
+
+    $database = new PDO('sqlite:../db/EAMs.db');
+    $database->exec("DROP TABLE IF EXISTS EAMS"); // dropping EAMS table (if it exists)
+
+    $create_table_query = "CREATE TABLE IF NOT EXISTS EAMS (rowID INTEGER PRIMARY KEY,"; // the create table script
+    $insert_table_query = "INSERT INTO EAMS ("; // the insert script
+
+    $highest_column = $worksheet->getHighestColumn();
+    $highest_row = $worksheet->getHighestRow();
+
+    /**
+     * First Row
+     */
+    $first_row = $worksheet->getRowIterator(1)->current();
+    $unparsed_cells = $first_row->getCellIterator();
+    $unparsed_cells->setIterateOnlyExistingCells(FALSE);
+
+    $values = "VALUES (";
+    foreach ($unparsed_cells as $cell)
     {
-        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
-        $reader->setReadDataOnly(TRUE);
-        $spreadsheet = $reader->load("../uploads/EAMs.xlsx");
-        $worksheet = $spreadsheet->getActiveSheet();
-
-        $database = new PDO('sqlite:../db/EAMs.db');
-        $database->exec("DROP TABLE IF EXISTS EAMS"); // dropping EAMS table (if it exists)
-
-        $first_row = true;
-        $row_count = 0;		// used to count amount of rows
-        $column_count = 0;	// used to count number of columns
-        $row_iteration = 0;	// used to keep track of iteration in loop below
-        $create_table_query = "CREATE TABLE IF NOT EXISTS EAMS (rowID INTEGER PRIMARY KEY,"; // the create table script
-        $insert_table_query = "INSERT INTO EAMS ("; // the insert script
-
-        foreach ($worksheet->getRowIterator() as $row)
-        {
-            $row_count++; // setting the amount of rows (corresponding to excel file)
-        }
-
-        foreach ($worksheet->getRowIterator() as $row)
-        {
-            $unparsed_cells = $row->getCellIterator();
-            $unparsed_cells->setIterateOnlyExistingCells(FALSE);
-            $cell_iteration = 0;
-            $row_iteration++;	// incrementing current row in for loop
-
-            if ($first_row)
-            { // if this is our first row...
-                foreach ($unparsed_cells as $cell)
-                {
-                    $column_count++; // setting the amount of columns (corresponding to excel file)
-                }
-
-                foreach ($unparsed_cells as $cell)
-                { // iterating through each cell in focused row
-                    $cell_iteration++; // incrementing focused cell
-                    $create_table_query .= "'" . $cell->getValue() . "'" . ($cell_iteration !== $column_count ? " TEXT, " : " TEXT)");
-                    $insert_table_query .= "'" . $cell->getValue() . "'" . ($cell_iteration !== $column_count ? ", " : ") ");
-                }
-
-                $insert_table_query .= "VALUES "; // concates this to our insert table script after loop finishes
-                $first_row = false; // no longer the first row
-            }
-            else
-            {
-                $insert_table_query .= "(";
-
-                foreach ($unparsed_cells as $cell)
-                {
-                    $cell_iteration++;
-                    $insert_table_query .= "'" . $cell->getValue() . "'" . ($cell_iteration !== $column_count ? ", " : ")");
-                }
-
-                $insert_table_query .= ($row_iteration !== $row_count ? ", " : "");
-            }
-        }
-
-        $database->exec($create_table_query);
-        $database->exec($insert_table_query);
-        $database->exec("DROP TABLE Excluded_Columns");
-        $database->exec("CREATE TABLE IF NOT EXISTS Excluded_Columns (rowID INTEGER PRIMARY KEY, Name TEXT)");
-        $database->exec("INSERT INTO Excluded_Columns (Name) VALUES ('rowID')");
+        $create_table_query .= "'" . $cell->getValue() . "'" . ($cell->getColumn() != $highest_column ? " TEXT, " : " TEXT)");
+        $insert_table_query .= "'" . $cell->getValue() . "'" . ($cell->getColumn() != $highest_column ? ", " : ") ");
+        $values .= ($cell->getColumn() != $highest_column ? "?, " : "?)");
     }
 
-	if (!(in_array($_FILES['file']['type'], $arr_file_types)))
-	{
-	  echo "false";
-	  return;
-	}
+    /**
+     * Other Rows
+     */
+    $database->exec($create_table_query); // creating table
+    $insert_table_query .= $values;
 
-	if (!file_exists('uploads'))
-	{
-	  mkdir('uploads', 0777);
-	}
+    $i = 1;
+    foreach($worksheet->getRowIterator(2) as $row)
+    {
+        $stmt = $database->prepare($insert_table_query);
+        $unparsed_cells = $row->getCellIterator();
+        $unparsed_cells->setIterateOnlyExistingCells(FALSE);
+        foreach ($unparsed_cells as $cell)
+        {
+            $stmt->bindValue($i, $cell->getValue());
+            $i++;
+        }
+        $stmt->execute();
+        $i = 1;
+    }
 
-	move_uploaded_file($_FILES['file']['tmp_name'], '../uploads/' . $_FILES['file']['name']);
-	echo "File uploaded successfully. You may safely leave the page.";
+    /**
+     * Inserting data...?
+     */
+    //$database->exec($insert_table_query);
+    $database->exec("DROP TABLE Excluded_Columns");
+    $database->exec("CREATE TABLE IF NOT EXISTS Excluded_Columns (rowID INTEGER PRIMARY KEY, Name TEXT)");
+    $database->exec("INSERT INTO Excluded_Columns (Name) VALUES ('rowID')");
+}
 
-    convert_excel_to_sqlite();
+if (!(in_array($_FILES['file']['type'], $arr_file_types)))
+{
+    echo "false";
+    return;
+}
+
+if (!file_exists('uploads'))
+{
+    mkdir('uploads', 0777);
+}
+
+move_uploaded_file($_FILES['file']['tmp_name'], '../uploads/' . $_FILES['file']['name']);
+echo "File uploaded successfully. You may safely leave the page.";
+
+convert_excel_to_sqlite();
